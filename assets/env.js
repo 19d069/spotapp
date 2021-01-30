cc842e45b34edb71ae7255d7772c060ac4eb5422
19d018 <78264080+19d018@users.noreply.github.com>
2021-01-31 00:24:38 +0900

    Update env.js

--- assets/env.js
+++ assets/env.js
@@ -1,3 +1,3 @@
 //SSID='1Z04hpGaUvTDULpC-sj6zBinnnndcCc4QXC6Q2A_F6QY'
-SSID='https://sheets.googleapis.com/v4/spreadsheets/1dfH2ctB4aNiEBWjVpZrTbe0xW4jincWNtaH6PWqkMS0/values/okazaki!A2:K1000?key=AIzaSyCBqZ8VftKevnjx-p_s8F3VEz4BXik9CeU'
+SSID='1dfH2ctB4aNiEBWjVpZrTbe0xW4jincWNtaH6PWqkMS0'
 SNAME=okazaki!A2:K1000
